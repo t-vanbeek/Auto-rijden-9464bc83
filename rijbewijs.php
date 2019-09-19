c324931ca1d0ff9d374109c691a0ecec969b5591
Thorvald van Beek <54794887+t-vanbeek@users.noreply.github.com>
2019-09-19 13:36:47 +0200

    Uitwerking van de exercise

---
+++ rijbewijs.php
@@ -0,0 +1,9 @@
+<?php
+$ja = 16.5;
+$var1 = readline("> Wat is je leeftijd? ");
+
+if ($var1 > $ja)
+    echo "> Je mag beginnen met rijlessen!";
+else
+echo "> Helaas, je mag nog niet beginnen met rijlessen";
+
